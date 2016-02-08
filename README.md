bd4a199730569a97737ec60f2b9b4ae87dc3ccab
Michael Silveira <gravitypersists@gmail.com>
2016-02-08 15:24:11 -0800

    initial project files

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # fancy-fetch
+
+## Usage
+
+```
+  import fetch from 'fancy-fetch'
+
+  fetch({
+    url: 'http://server.com/api/endpoint/or/something',
+    method: 'put',
+    body: {
+      keys: values
+    },
+    success: function() {},
+    error: function() {}
+  })
+```
+
+The only thing special about this helper is that it provides a `success` and `error` callback. All other options are passed along to isomorphic-fetch, which is another wrapper around github's polyfill for whatwg spec of fetch.
---
+++ index.js
@@ -0,0 +1,33 @@
+import fetch from 'isomorphic-fetch';
+
+export function fancyFetch(options) {
+  const { url, success, error, method, body, ...rest } = options;
+
+  const upperMethod = method && method.toUpperCase() || 'GET'
+  const bodySerialized =  body ? JSON.stringify(body) : null;
+
+  let opts = { ...rest, method: upperMethod };
+  if (opts.body) opts.body = JSON.stringify(opts.body);
+
+  fetch(url, opts)
+  .then(response => {
+    if (response.status >= 400) {
+      let err = new Error(`Could not ${options.method || 'get'} ${url}: ${response.statusText}`);
+      err.response = response;
+      throw err;
+    } else {
+      return response.json();
+    }
+  })
+  .then(json => success(json))
+  .catch(err => {
+    throw err;
+    if (err.response) {
+      err.response.json().then(json => error(json))
+    } else {
+      // TODO: This isn't always true - need to figure out a better way to handle catch
+      // when an error is occuring in the success callback
+      error({ networkFailure: true })
+    }
+  })
+}
